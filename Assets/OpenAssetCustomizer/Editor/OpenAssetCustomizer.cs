9e822c7fe6c003ed7406ca9cb92a43820d49c476
baba_s <shotababa1206@gmail.com>
2018-03-15 20:19:33 +0900

    初コミット

---
+++ Assets/OpenAssetCustomizer/Editor/OpenAssetCustomizer.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.Callbacks;
+
+namespace KoganeEditorLib
+{
+	public static class OpenAssetCustomizer
+	{
+		[OnOpenAsset]
+		private static bool OnOpenAsset( int instanceID, int line )
+		{
+			var assetPath = AssetDatabase.GetAssetPath( instanceID );
+			var assetExtension = Path
+				.GetExtension( assetPath )
+				.Replace( ".", string.Empty )
+			;
+
+			var scriptPath = AssetDatabase
+				.GetAllAssetPaths()
+				.FirstOrDefault( c => c.EndsWith( "OpenAssetCustomizer.cs" ) )
+			;
+			var dir = Path.GetDirectoryName( scriptPath );
+			var path = string.Format( "{0}/Settings.asset", dir );
+			var settings = AssetDatabase.LoadAssetAtPath<OpenAssetSettings>( path );
+			var list = settings.List;
+			var data = list.FirstOrDefault( c => c.Extension == assetExtension );
+
+			if ( data != null )
+			{
+				var process = Process.Start( data.ApplicationPath, assetPath );
+				return true;
+			}
+
+			return false;
+		}
+	}
+}
---
+++ Assets/OpenAssetCustomizer/Editor/OpenAssetSettingsDataDrawer.cs
@@ -0,0 +1,46 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace KoganeEditorLib
+{
+	[CustomPropertyDrawer( typeof( OpenAssetSettingsData ) )]
+	public sealed class OpenAssetSettingsDataDrawer : PropertyDrawer
+	{
+		public override void OnGUI( Rect position, SerializedProperty property, GUIContent label )
+		{
+			using ( new EditorGUI.PropertyScope( position, label, property ) )
+			{
+				position.height = EditorGUIUtility.singleLineHeight;
+
+				var applicationPathRect = new Rect( position )
+				{
+					width = position.width - 18,
+				};
+				var dialogRect = new Rect( position )
+				{
+					x = applicationPathRect.xMax + 2,
+					width = 16,
+				};
+				var extensionRect = new Rect( position )
+				{
+					y = applicationPathRect.yMax + 2,
+				};
+
+				var applicationPathProperty = property.FindPropertyRelative( "m_applicationPath" );
+				var extensionProperty = property.FindPropertyRelative( "m_extension" );
+
+				applicationPathProperty.stringValue = EditorGUI.TextField( applicationPathRect, applicationPathProperty.displayName, applicationPathProperty.stringValue );
+				extensionProperty.stringValue = EditorGUI.TextField( extensionRect, extensionProperty.displayName, extensionProperty.stringValue );
+
+				if ( GUI.Button( dialogRect, GUIContent.none, "ShurikenDropdown" ) )
+				{
+					var path = EditorUtility.OpenFilePanel( "Application Path", "", "" );
+					if ( !string.IsNullOrEmpty( path ) )
+					{
+						applicationPathProperty.stringValue = path;
+					}
+				}
+			}
+		}
+	}
+}
